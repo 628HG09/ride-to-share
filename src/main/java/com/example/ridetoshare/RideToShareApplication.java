28e0b3ab2628b5223bef00476198eafc0bfb4135
SRutges <104904365+628HG09@users.noreply.github.com>
2023-02-07 20:17:48 +0100

    basis project

---
+++ src/main/java/com/example/ridetoshare/RideToShareApplication.java
@@ -0,0 +1,13 @@
+package com.example.ridetoshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideToShareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RideToShareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ridetoshare/RideToShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ridetoshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideToShareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
